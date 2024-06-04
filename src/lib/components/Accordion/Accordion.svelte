061ee7c500fd8cac076272213886f88005dccbd6
morde <mordechai.hadad01@gmail.com>
2024-06-04 14:39:21 +0300

    added accordion

---
+++ src/lib/components/Accordion/Accordion.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { writable, type Writable } from 'svelte/store';
+	import { setContext } from 'svelte';
+	import type { context } from './context.ts';
+	
+	let context: Writable<context> = writable({
+		isExpanded: false,
+	});
+
+	setContext('accordion', context);
+
+	const toggle = () => {
+		context.update(value => ({ isExpanded: !value.isExpanded }));
+	};
+</script>
+
+<button
+	on:click={toggle}
+	class="accordion {$$props.class}"
+>
+    <slot />
+</button>
+
+<style>
+	.accordion {
+		display: flex;
+		flex-direction: column;
+	}
+</style>
---
+++ src/lib/components/Accordion/AccordionContent.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import { slide } from 'svelte/transition';
+	import type { context } from './context.js';
+	import type { Writable } from 'svelte/store';
+
+	let context: Writable<context> = getContext('accordion');
+	export let transition = slide;
+</script>
+
+{#if $context.isExpanded}
+	<div transition:transition>
+		<slot />
+	</div>
+{/if}
