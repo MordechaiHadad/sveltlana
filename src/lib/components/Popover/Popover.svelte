7024ef2c62cfc83e0745e0574ac70df5408da1ba
morde <mordechai.hadad01@gmail.com>
2024-06-18 18:08:52 +0300

    added: close on outside

--- src/lib/components/Popover/Popover.svelte
+++ src/lib/components/Popover/Popover.svelte
@@ -1,16 +1,29 @@
 <script lang="ts">
 	import { writable, type Writable } from 'svelte/store';
-    import type { context } from './context.js';
+	import type { context } from './context.js';
 	import { twMerge } from 'tailwind-merge';
 	import { setContext } from 'svelte';
+	import { clickOutside } from '$lib/functions.js';
 
-    let context: Writable<context> = writable({
-        isExpanded: false
-    });
+	export let closeOnOutsideClick = true;
+
+	let context: Writable<context> = writable({
+		isExpanded: false
+	});
 
 	setContext('popover', context);
+
+	const close = () => {
+		context.update((value) => ({ isExpanded: false }));
+	};
 </script>
 
-<div class={twMerge("relative inline-block", $$props.class)} tabindex="-1">
-    <slot />
+<div
+	class={twMerge('relative inline-block', $$props.class)}
+	tabindex="-1"
+	use:clickOutside={() => {
+		if (closeOnOutsideClick) close();
+	}}
+>
+	<slot />
 </div>
