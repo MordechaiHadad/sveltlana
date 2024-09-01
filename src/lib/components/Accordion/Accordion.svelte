8cbc857f7c8fa2c39cee531605b6840a5a088da9
morde <mordechai.hadad01@gmail.com>
2024-09-01 11:42:25 +0300

    very nice new thing

--- src/lib/components/Accordion/Accordion.svelte
+++ src/lib/components/Accordion/Accordion.svelte
@@ -1,23 +1,30 @@
 <script lang="ts">
 	import { writable, type Writable } from 'svelte/store';
-	import { setContext } from 'svelte';
+	import { onDestroy, setContext } from 'svelte';
 	import type { context } from './context.ts';
 	import { twMerge } from 'tailwind-merge';
-	
+
 	let context: Writable<context> = writable({
-		isExpanded: false,
+		isExpanded: false
 	});
 
 	setContext('accordion', context);
 
 	const toggle = () => {
-		context.update(value => ({ isExpanded: !value.isExpanded }));
+		context.update((value) => ({ isExpanded: !value.isExpanded }));
 	};
+
+	let isExpanded = false;
+
+	const unsubscribe = context.subscribe((value) => {
+		isExpanded = value.isExpanded;
+	});
+
+	onDestroy(() => {
+		unsubscribe();
+	});
 </script>
 
-<button
-	on:click={toggle}
-	class={twMerge("flex flex-col", $$props.class)}
->
-    <slot />
+<button on:click={toggle} class={twMerge('flex flex-col', $$props.class)}>
+	<slot {isExpanded} />
 </button>
